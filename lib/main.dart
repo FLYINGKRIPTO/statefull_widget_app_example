c04700228830f4aafd1d641f0dac355b9ad9d89c
FLYINGKRIPTO <srivastava.shivanshu4@gmail.com>
2019-09-10 15:32:45 +0530

    dropdown menu done

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
-     title: "Statefull app example",
-     home: FavouriteCity(),
+    title: "Statefull app example",
+    home: FavouriteCity(),
   ));
 }
-class FavouriteCity extends StatefulWidget{
+
+class FavouriteCity extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return _FavouriteCityState();
   }
 }
-class _FavouriteCityState extends State<FavouriteCity>{
 
-   String nameCity=" ";
+class _FavouriteCityState extends State<FavouriteCity> {
+  String nameCity = " ";
+  var _currencies = ['Rupees','Dollar','Pounds','Others'];
+  var _currentItemSelected = 'Rupees';
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    debugPrint("Favourite city widget is created");
     return Scaffold(
       appBar: AppBar(
         title: Text("Stateful APP Example"),
@@ -28,22 +32,39 @@ class _FavouriteCityState extends State<FavouriteCity>{
         child: Column(
           children: <Widget>[
             TextField(
-              onSubmitted: (String userInput){
+              onChanged: (String userInput) {
                 setState(() {
                   nameCity = userInput;
                 });
               },
             ),
+
+            DropdownButton<String>(
+              items: _currencies.map((String dropDownStringItem) {
+                return DropdownMenuItem<String>(
+                  value: dropDownStringItem,
+                  child: Text(dropDownStringItem),
+
+                );
+              }).toList(),
+
+              onChanged: (String newValueSelected){
+               setState(() {
+                 this._currentItemSelected = newValueSelected;
+               });
+              },
+
+              value: _currentItemSelected,
+            ),
             Padding(
                 padding: EdgeInsets.all(30.0),
                 child: Text(
-                "Your favourite city is  $nameCity",
-                 style: TextStyle(fontSize: 20.0),
-            ))
+                  "Your favourite city is  $nameCity",
+                  style: TextStyle(fontSize: 20.0),
+                ))
           ],
         ),
       ),
     );
   }
-
 }
